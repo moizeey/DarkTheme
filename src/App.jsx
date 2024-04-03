232d0af0f0080e0caff9f99bf679d5d179f030ee
moizeey <moizeey17@gmail.com>
2024-04-04 00:39:24 +0500

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
     <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center dark:bg-zinc-900">
         <div className="w-full">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <div className="w-full max-w-sm mx-auto flex items-center justify-center mb-4">
             <ThemeBtn />
           </div>
           <div className="w-full max-w-sm   mx-auto">
